3e9eaf42d60d3fb79ac8e91208d908ff23df623e
AbelTarazona <abel.tarazona@urp.edu.pe>
2023-01-06 14:06:25 -0500

    removing theme

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Applaudo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: const HomePage(),
     );
   }
