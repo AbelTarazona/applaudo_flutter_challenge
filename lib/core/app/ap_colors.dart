7df52f5904892b58abc3ab38ca1db654743b42a1
AbelTarazona <abel.tarazona@urp.edu.pe>
2023-01-06 20:41:06 -0500

    adding colors

---
+++ lib/core/app/ap_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class APColors {
+  Color licorice = const Color(0xFF292B35);
+  Color smoke = const Color(0xFFF8F8F8);
+  Color comet = const Color(0xFF575767);
+  Color ghost = const Color(0xFFB9B9BE);
+  Color dodgerBlue = const Color(0xFF1294F2);
+}
